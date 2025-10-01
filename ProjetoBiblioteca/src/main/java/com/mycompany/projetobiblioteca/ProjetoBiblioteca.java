feeb8214a7a4b3e3d4911d0a2385562e03eae30c
Everton Tavares <44384812+Ever137@users.noreply.github.com>
2025-09-30 23:33:43 -0300

    Add files via upload
    Projeto de Prog 3

---
+++ ProjetoBiblioteca/src/main/java/com/mycompany/projetobiblioteca/ProjetoBiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetobiblioteca;
+
+/**
+ *
+ * @author anton
+ */
+public class ProjetoBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
